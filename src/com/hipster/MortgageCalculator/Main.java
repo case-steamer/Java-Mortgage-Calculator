9344996a835d43db47c5c1cd0e6b2d505baf448f
James Haining <hipster1911@gmail.com>
2025-10-21 16:14:42 -0500

    Added error handling

--- src/com/hipster/MortgageCalculator/Main.java
+++ src/com/hipster/MortgageCalculator/Main.java
@@ -10,15 +10,30 @@ public class Main {
         Scanner principleScan = new Scanner(System.in);
         Scanner interestScan = new Scanner(System.in);
         Scanner yearScan = new Scanner(System.in);
+
         int principle = 0;
          do {
             System.out.print("What is the principle? ");
             principle = principleScan.nextInt();
+            if (principle < 1000 || principle > 1000000)
+                System.out.println("Not a valid number. Enter a number between 1,000 & 1,000,000: ");
         } while (principle < 1000 || principle > 1000000);
-        System.out.print("What is the annual interest rate? ");
-        double interestRate = interestScan.nextDouble();
-        System.out.println("What is the term of the mortgage? ");
-        int term = yearScan.nextInt();
+
+         double interestRate = 0;
+         do {
+             System.out.print("What is the annual interest rate? ");
+             interestRate = interestScan.nextDouble();
+             if (interestRate <= 0 || interestRate > 30)
+                 System.out.println("Not a valid number. Enter a number between 0 & 30: ");
+         } while (interestRate <= 0 || interestRate > 30);
+
+         int term = 0;
+         do {
+             System.out.println("What is the term of the mortgage? ");
+             term = yearScan.nextInt();
+             if (term <= 0 || term > 30)
+                 System.out.println("Not a valid number. Enter a number between 0 & 30: ");
+         } while (term <= 0 || term > 30);
 
         Calculator myRate = new Calculator(principle, interestRate, term);
         double monthlyPayment = myRate.calculateRate();
