3142fb66d5db696ed2baf02ef8e7066bee7ee28c
James Haining <hipster1911@gmail.com>
2025-10-27 13:46:04 -0500

    refactored input code as function instances

--- src/com/hipster/MortgageCalculator/Main.java
+++ src/com/hipster/MortgageCalculator/Main.java
@@ -7,38 +7,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner principleScan = new Scanner(System.in);
-        Scanner interestScan = new Scanner(System.in);
-        Scanner yearScan = new Scanner(System.in);
 
-        int principle = 0;
-         do {
-            System.out.print("What is the principle? ");
-            principle = principleScan.nextInt();
-            if (principle < 1000 || principle > 1000000)
-                System.out.println("Not a valid number. Enter a number between 1,000 & 1,000,000: ");
-        } while (principle < 1000 || principle > 1000000);
+        int principle = (int) readNumber("What is the principle? ", 1, 1_000_000);
 
-         double interestRate = 0;
-         do {
-             System.out.print("What is the annual interest rate? ");
-             interestRate = interestScan.nextDouble();
-             if (interestRate <= 0 || interestRate > 30)
-                 System.out.println("Not a valid number. Enter a number between 0 & 30: ");
-         } while (interestRate <= 0 || interestRate > 30);
+        double interestRate = readNumber("What is the annual interest rate? ", 0, 30);
 
-         int term = 0;
-         do {
-             System.out.println("What is the term of the mortgage? ");
-             term = yearScan.nextInt();
-             if (term <= 0 || term > 30)
-                 System.out.println("Not a valid number. Enter a number between 0 & 30: ");
-         } while (term <= 0 || term > 30);
+        int term = (int) readNumber("What is the term of the mortgage? ", 0, 30);
 
         Calculator myRate = new Calculator(principle, interestRate, term);
         double monthlyPayment = myRate.calculateRate();
         DecimalFormat df = new DecimalFormat("0.00");
         String mp = df.format(monthlyPayment);
         System.out.println("Your monthly payment is $" + mp);
+    }
+
+
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        do {
+            System.out.print(prompt);
+            value = scanner.nextDouble();
+            if (value <= min || value > max)
+                System.out.println("Not a valid number. Enter a number between " + min + " and " + max + ": ");
+        } while (value <= min || value > max);
+        return value;
     }
 }
